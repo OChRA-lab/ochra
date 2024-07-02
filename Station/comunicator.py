9363e9bfd9d6cc96ab6aa2995fafced4a1518e97
acedwards7 <adamcedwards1@gmail.com>
2024-07-02 10:46:41 +0100

    added more constructor arguments and started on notification type startup

--- Station/comunicator.py
+++ Station/comunicator.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 import uvicorn
 from typing import Dict, Any, Optional
 from OChRA_Common.connections.db_connection import DbConnection
+from OChRA_Common.connections.lab_connection import LabConnection
+from OChRA_Common.operations import *
 
 
 class operationExecute(BaseModel):
@@ -13,28 +15,43 @@ class operationExecute(BaseModel):
 
 
 class Comunicator:
-    def __init__(self) -> None:
+    def __init__(self,
+                 dbip="138.253.124.144:27017",
+                 host_ip="0.0.0.0",
+                 port=8000,
+                 lab_ip="10.24.169.42") -> None:
+        self.lab_conn: LabConnection = LabConnection(lab_ip)
         self.app = FastAPI()
         self.router = APIRouter()
+        self.host_ip = host_ip
+        self.port = port
         self.devices = []
         self.router.add_api_route(
             "/process_op", self.process_operation, methods=["POST"])
         self.router.add_api_route("/ping", self.ping, methods=["GET"])
         self.app.include_router(self.router)
-        self.db_conn = DbConnection("138.253.124.144:27017")
+        self.db_conn = DbConnection(dbip)
 
     def run(self):
-        uvicorn.run(self.app, host="0.0.0.0")
+        uvicorn.run(self.app, host=self.host_ip, port=self.port)
 
     def ping(self, request: Request):
         clientHost = request.client.host
         print(clientHost)
         return
 
+    def start_up(self):
+        station_id = self.lab_conn.create_station()
+        for device in self.devices:
+            id = self.lab_conn.construct_object(
+                device.__class__.__name__, "devices", **device.__dict__)
+            self.lab_conn.add_device_to_station(station_id, id)
+
     def process_operation(self, args: operationExecute):
         try:
             for i in self.devices:
                 if i.name == args.deviceName:
-                    return i.execute(args.operation, **args.args)
+                    op = eval(f"{args.operation}()")
+                    return i.execute(op, **args.args)
         except Exception as e:
             raise HTTPException(500, detail=str(e))
