c2630d292b47ca0ce1d9cf8fe5a519ebc5e47748
bt-nav <bjtnav@gmail.com>
2024-07-22 16:16:58 +0100

    fixed arguments

--- tests/lab_connection_test.py
+++ tests/lab_connection_test.py
@@ -18,17 +18,17 @@ def lab_connection():
 def test_construct_object(lab_connection):
     lab_conn, mock_rest_adapter = lab_connection
     mock_rest_adapter.construct.return_value = "mocked_construct_result"
-    result = lab_conn.construct_object("test_type", "test_module", "test_value")
+    result = lab_conn.construct_object("test_type", "test_module")
     mock_rest_adapter.construct.assert_called_once_with(
-        "test_type", "test_module", "test_value")
+        "test_type", "test_module")
     assert result == "mocked_construct_result"
     
 def test_call_on_object(lab_connection):
     lab_conn, mock_rest_adapter = lab_connection
     mock_rest_adapter.call.return_value = "mocked_call_result"
-    result = lab_conn.call_on_object("test_id", "test_function", "test_value")
+    result = lab_conn.call_on_object("test_id", "test_function")
     mock_rest_adapter.call.assert_called_once_with(
-        "test_id", "test_function", "test_value")
+        "test_id", "test_function")
     assert result == "mocked_call_result"
 
 def test_get_object(lab_connection):
@@ -41,8 +41,8 @@ def test_get_object(lab_connection):
 def test_patch_object(lab_connection):
     lab_conn, mock_rest_adapter = lab_connection
     mock_rest_adapter.patch.return_value = "mocked_patch_result"
-    result = lab_conn.patch_object("test_id", "test_value")
-    mock_rest_adapter.patch.assert_called_once_with("test_id", "test_value")
+    result = lab_conn.patch_object("test_id")
+    mock_rest_adapter.patch.assert_called_once_with("test_id")
     assert result == "mocked_patch_result"
     
 def test_create_station(lab_connection):
