a6b258bc63139a76abf5659c41bfa8c31a1b068c
bt-nav <bjtnav@gmail.com>
2024-07-24 10:30:40 +0100

    added test_construct_object which passes successfully

--- tests/test_lab_connection.py
+++ tests/test_lab_connection.py
@@ -12,3 +12,17 @@ def lab_connection():
         lab_conn = LabConnection(hostname="test_host", api_key="test_key", ssl_verify=True, logger=None)
     return lab_conn, mock_rest_adapter
 
+def test_construct_object(lab_connection):
+    lab_conn, mock_rest_adapter = lab_connection
+    mock_rest_adapter.post.return_value = MagicMock(data="mocked_construct_result")
+    result = lab_conn.construct_object(
+        object_type=str, catalogue_module="test_module", param="test_value")
+    mock_rest_adapter.post.assert_called_once_with(
+        endpoint="object/construct",
+        data={
+            "object_type": "str",
+            "catalogue_module": "test_module",
+            "contstructor_params": {"param": "test_value"}
+        }
+    )
+    assert result == "mocked_construct_result"
