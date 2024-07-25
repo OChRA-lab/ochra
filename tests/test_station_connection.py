196d95689516472cc35a54a4cc89af49b118dfbc
bt-nav <bjtnav@gmail.com>
2024-07-25 16:18:18 +0100

    removed unnecessary import

--- tests/test_station_connection.py
+++ tests/test_station_connection.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 from ochra_common.connections.station_connection import StationConnection
 from ochra_common.connections.rest_adapter import LabEngineException
 
