268d1ea9da17f38abf514435d94ef6e63e02b893
acedwards7 <adamcedwards1@gmail.com>
2024-07-26 14:34:16 +0100

    moved code to post init

--- ochra_common/utils/db_decorator.py
+++ ochra_common/utils/db_decorator.py
@@ -1,38 +1,51 @@
 from dataclasses import fields
-from ochra_common.connections.db_connection import DbConnection
+from OChRA_Common.ochra_common.connections.db_connection import DbConnection
+from OChRA_Common.ochra_common.connections.lab_connection import LabConnection
+from OChRA_Common.ochra_common.operations.operationModels import OperationDbModel
 
 
 def backend_db(cls):
-    """Decorator for replacing method calls for backend db interaction 
+    """Decorator for replacing method calls for backend db interaction
     """
 
     def pre_init(self, **kwargs):
-        self._db = DbConnection()
+        self._db_conn = DbConnection()
+        print("run pre init")
 
+    def post_init(self, **kwargs):
+        self.id = self._db_conn.create(
+            self.collection_name, self)
+        print("run post init")
+        for field in fields(cls):
+            # skip over id and collection_name
+            if field.name == "id":
+                continue
+            if field.name == "collection_name":
+                continue
+            # Create custom getter
+
+            def getter(self, name=field.name):
+                value = self._db_conn.read(self.collection_name, self.id, name)
+                print(f"Getting {name}: {value}")
+                return value
+
+            # Create custom setter
+            def setter(self, value, name=field.name):
+                print(self.id, self.collection_name)
+                value = self._db_conn.update(
+                    self.collection_name, self.id, {str(name): value})
+                print(f"Setting {name} to: {value}")
+
+            # Set the property on the class with the custom getter and setter
+            setattr(cls, field.name, property(getter, setter))
     original_init = cls.__init__
 
     def new_init(self, *args, **kwargs):
         pre_init(self, **kwargs)
         original_init(self, *args, **kwargs)
+        post_init(self, **kwargs)
 
     cls.__init__ = new_init
-
-    # Loop through all fields defined in the dataclass
-    for field in fields(cls):
-
-        # Create custom getter
-        def getter(self, name=field.name):
-            value = self._db.read(name)
-            print(f"Getting {name}: {value}")
-            return value
-
-        # Create custom setter
-        def setter(self, value, name=field.name):
-            value = self._db.update(name, value)
-            print(f"Setting {name} to: {value}")
-
-        # Set the property on the class with the custom getter and setter
-        setattr(cls, field.name, property(getter, setter))
 
     return cls
 
