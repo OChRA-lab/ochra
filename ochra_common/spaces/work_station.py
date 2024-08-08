6786dedc22ff35d856975e3cdfad06232ad46c86
acedwards7 <adamcedwards1@gmail.com>
2024-08-08 11:19:30 +0100

    forgot to dataclass

--- ochra_common/spaces/work_station.py
+++ ochra_common/spaces/work_station.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from ochra_common.base import DataModel
-from ochra_common.spaces.location import Location
 from ochra_common.spaces.station import Station
 from ochra_common.equipment.device import Device
 
-class WorkStation(DataModel,ABC,Station):
+
+@dataclass
+class WorkStation(DataModel, ABC, Station):
     devices: list[Device]
 
     @abstractmethod
