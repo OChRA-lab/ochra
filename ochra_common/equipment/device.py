f0c7f765b45c2aa42085450e24e6d361f6dd9148
acedwards7 <adamcedwards1@gmail.com>
2024-08-08 13:02:45 +0100

    device

---
+++ ochra_common/equipment/device.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from ochra_common.base import DataModel
+from ochra_common.equipment.operation import Operation
+from ochra_common.storage.inventory import Inventory
+from enum import Enum
+
+@dataclass
+class Device(DataModel, ABC):
+    name: str
+    status: Enum
+    operation_history: list[Operation]
+    inventory: Inventory
+
+    @abstractmethod
+    def __init__(self,**kwargs)->None:
+        super().__init__(**kwargs)
