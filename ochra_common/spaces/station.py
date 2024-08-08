7d247f57da53614c99c94f8b52f4dc6d79f2feaa
acedwards7 <adamcedwards1@gmail.com>
2024-08-08 11:15:33 +0100

    station

---
+++ ochra_common/spaces/station.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from ochra_common.base import DataModel
+from ochra_common.spaces.location import Location
+from ochra_common.storage.stock import Stock
+
+@dataclass
+class Station(DataModel, ABC):
+    name: str
+    location: Location
+    stock: Stock
