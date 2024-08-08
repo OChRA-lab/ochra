ee00e7c5a9fc1c3bbd5d78706a3dd669e2f81e0e
acedwards7 <adamcedwards1@gmail.com>
2024-08-08 13:11:14 +0100

    operation + operation result

---
+++ ochra_common/equipment/operation.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from ochra_common.base import DataModel
+import uuid
+from enum import Enum
+from datetime import datetime
+from ochra_common.equipment.operation_result import OperationResult
+
+@dataclass
+class Operation(DataModel, ABC):
+    _caller: uuid
+    _method: str
+    _args: dict
+    status: Enum
+    start_timestamp: datetime
+    end_timestamp: datetime
+    result: list[OperationResult]
