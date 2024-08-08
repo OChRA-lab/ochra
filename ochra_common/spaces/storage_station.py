815e9e2a0eacdf3ed3e73367845d043b85c9374b
acedwards7 <adamcedwards1@gmail.com>
2024-08-08 11:20:09 +0100

    storage station

---
+++ ochra_common/spaces/storage_station.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from ochra_common.base import DataModel
+from ochra_common.spaces.location import Location
+from ochra_common.spaces.station import Station
+from ochra_common.equipment.device import Device
+
+@dataclass
+class StorageStation(ABC,Station,DataModel):
+    pass
