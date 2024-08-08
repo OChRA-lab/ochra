6da955e68771c6291dbfcb4ef825addf389fee7d
acedwards7 <adamcedwards1@gmail.com>
2024-08-08 11:13:32 +0100

    lab

---
+++ ochra_common/spaces/lab.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, asdict
+from ochra_common.base import DataModel
+from ochra_common.spaces.station import Station
+from ochra_common.agents.agent import Agents
+from ochra_common.agents.robot import Robot
+from ochra_common.agents.scientist import Scientist
+
+
+@dataclass
+class Lab(DataModel, ABC):
+    stations: list[Station]
+    agents: list[Agents]
+
+    @abstractmethod
+    def get_robots(self) -> list[Robot]:
+        pass
+
+    @abstractmethod
+    def getrobot(self, robot: str | type) -> Robot:
+        pass
+
+    @abstractmethod
+    def get_scientists(self) -> list[Scientist]:
+        pass
+
+    @abstractmethod
+    def add_scientist(self, scientist: Scientist) -> bool:
+        pass
