50eafabb0b39a391daec9a0d5fd129e261b218c1
bt-nav <bjtnav@gmail.com>
2024-08-08 14:00:31 +0100

    added agent class

---
+++ ochra_common/agents/agent.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from ochra_common.base import DataModel
+from ochra_common.agents.task import Task
+from enum import Enum
+
+
+@dataclass
+class Agent(DataModel,ABC):
+    name: str
+    status: Enum
+    assigned_task: Task
+    task_status: Enum
+    tasks_history: list[Task]
+    
+    @abstractmethod
+    def assign(Task) -> bool:
+        pass
+        
