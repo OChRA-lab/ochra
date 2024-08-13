26e59281707929fff7a1956a762a6628eb7723ff
acedwards7 <adamcedwards1@gmail.com>
2024-08-13 13:55:36 +0100

    removed execute

--- ochra_common/agents/robot.py
+++ ochra_common/agents/robot.py
@@ -1,15 +1,9 @@
-from abc import abstractmethod
 from dataclasses import dataclass
 from ochra_common.spaces.location import Location
 from ochra_common.agents.agent import Agent
-from ochra_common.agents.robot_task import RobotTask
 
 
 @dataclass
 class Robot(Agent):
     type: str
     location: Location
-
-    @abstractmethod
-    def execute(self, robot_task: RobotTask) -> bool:
-        pass
