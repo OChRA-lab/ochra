f516cce9597aa74d55eeb332c1e7af563dfa845f
acedwards7 <adamcedwards1@gmail.com>
2024-08-13 13:52:14 +0100

    agent not agents

--- ochra_common/spaces/lab.py
+++ ochra_common/spaces/lab.py
@@ -1,8 +1,8 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from dataclasses import dataclass
 from ochra_common.base import DataModel
 from ochra_common.spaces.station import Station
-from ochra_common.agents.agent import Agents
+from ochra_common.agents.agent import Agent
 from ochra_common.agents.robot import Robot
 from ochra_common.agents.scientist import Scientist
 
@@ -10,7 +10,7 @@ from ochra_common.agents.scientist import Scientist
 @dataclass
 class Lab(DataModel):
     stations: list[Station]
-    agents: list[Agents]
+    agents: list[Agent]
 
     @abstractmethod
     def get_robots(self) -> list[Robot]:
