6424734dc354c568147aed78516a7f2fe9785793
acedwards7 <adamcedwards1@gmail.com>
2024-08-13 13:57:16 +0100

    doesnt need datamodel

--- ochra_common/agents/robot_task.py
+++ ochra_common/agents/robot_task.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass
-from ochra_common.base import DataModel
 from ochra_common.agents.task import Task
 from enum import Enum
 
 
 @dataclass
-class RobotTask(DataModel, Task):
+class RobotTask(Task):
     priority: Enum
