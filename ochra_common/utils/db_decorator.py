b4e7f23b8666710385e9a87bb260d837262d2be2
acedwards7 <adamcedwards1@gmail.com>
2024-08-19 12:53:16 +0100

    reworked to use new structure

--- ochra_common/utils/db_decorator.py
+++ ochra_common/utils/db_decorator.py
@@ -2,6 +2,8 @@ from dataclasses import fields
 from ochra_common.connections.db_connection import DbConnection
 from ochra_common.connections.lab_connection import LabConnection
 from ochra_common.operations.operationModels import Operation
+from ochra_common.equipment.device import Device
+from dataclasses import asdict
 
 
 def backend_db(cls):
@@ -13,31 +15,39 @@ def backend_db(cls):
         print("run pre init")
 
     def post_init(self, **kwargs):
-        self.db_data["id"] = self._db_conn.create(self.db_data, self)
+        self._db_conn.create(self.to_dict(), kwargs)
         print("run post init")
         for field in fields(self):
             # skip over db stuff
-            if field.name in ["db_data"]:
+            if field.name in ["id"]:
+                continue
+            if field.name.startswith("_"):
+                continue
+            if field.metadata.get("backend_only"):
                 continue
             # Create custom getter
 
             def getter(self, name=field.name):
                 value = self._db_conn.read(
-                    self.db_data, name)
+                    {"id": self.id.hex,
+                     "_cls": self._cls,
+                     "_collection": self._collection}, name)
                 print(f"Getting {name}: {value}")
                 return value
 
             # Create custom setter
             def setter(self, value, name=field.name):
                 value = self._db_conn.update(
-                    self.db_data, {str(name): value})
+                    {"id": self.id.hex,
+                     "_cls": self._cls,
+                     "_collection": self._collection}, {str(name): value})
                 print(f"Setting {name} to: {value}")
 
             # Set the property on the class with the custom getter and setter
             setattr(cls, field.name, property(getter, setter))
-            if field.name in kwargs:
-                att = getattr(cls, field.name)
-                att.fset(self, kwargs[field.name])
+            # if field.name in kwargs:
+            #    att = getattr(cls, field.name)
+            #    att.fset(self, kwargs[field.name])
 
     original_init = cls.__init__
 
@@ -59,19 +69,26 @@ def frontend_db(cls):
 
     def post_init(self, **kwargs):
         # Loop through all fields defined in the dataclass
-        self.db_data["id"] = self._lab_conn.construct_object(
-            cls, "operations",
-            **kwargs
-        )
+        if issubclass(cls, Device):
+            self._lab_conn.construct_object(
+                Device, "Devices",
+                **self.to_dict()
+            )
         for field in fields(cls):
             # skip over db stuff
-            if field.name in ["db_data"]:
+            if field.name in ["id", "db_data"]:
+                continue
+            if field.name.startswith("_"):
+                continue
+            if field.metadata.get("backend_only"):
                 continue
 
             # Create custom getter
             def getter(self, name=field.name):
                 value = self._db_conn.read(
-                    self.db_data, name)
+                    {"id": self.id.hex,
+                     "_cls": self._cls,
+                     "_collection": self._collection}, name)
                 print(f"Getting {name}: {value}")
                 return value
 
