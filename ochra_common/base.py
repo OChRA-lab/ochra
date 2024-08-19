cb4ba778a553dd875f71c6d535c2081fd7cc04f6
acedwards7 <adamcedwards1@gmail.com>
2024-08-19 12:37:57 +0100

    added json helper methods, default factory for id and to dict/to_json methods

--- ochra_common/base.py
+++ ochra_common/base.py
@@ -1,9 +1,18 @@
 import uuid
 from abc import ABC
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
+import json
 
 
-@dataclass
+def is_jsonable(x):
+    try:
+        json.dumps(x)
+        return True
+    except (TypeError, OverflowError):
+        return False
+
+
+@dataclass(kw_only=True)
 class DataModel(ABC):
     """
     DataModel class that serves as a base for all dataclasses that are to be stored in the database.
@@ -13,15 +22,31 @@ class DataModel(ABC):
         _collection (str): The name of the collection where the data model will be stored.
         _cls (str): The class name of the data model.
     """
-    id: uuid.UUID = uuid.uuid4()
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
     _collection: str
     _cls: str
 
     def to_json(self):
+        """Convert the data model instance to a JSON string.
+
+        Returns:
+            str: json string representation of the data model instance.
+        """
+
+        selfdict = self.to_dict()
+        return json.dumps(selfdict)
+
+    def to_dict(self):
         """
         Convert the data model instance to a JSON-serializable dictionary.
 
         Returns:
             dict: A dictionary representation of the data model instance.
         """
-        return asdict(self)
+        selfdict = asdict(self)
+        for key, value in selfdict.items():
+            if isinstance(value, uuid.UUID):
+                selfdict[key] = value.hex
+            elif not is_jsonable({key: value}):
+                selfdict[key] = value.to_json()
+        return selfdict
