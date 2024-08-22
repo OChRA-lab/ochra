fa586c4ca069b8bcb6d6d43b9e943c1bd7c63e0d
acedwards7 <adamcedwards1@gmail.com>
2024-08-22 12:48:29 +0100

    updated to match new model

--- ochra_manager/lab/models/device.py
+++ ochra_manager/lab/models/device.py
@@ -8,8 +8,4 @@ class Device(AbstractDevice):
         super().__init__(
             name=kwargs["name"],
             status=kwargs["status"],
-            _cls=kwargs["_cls"],
-            _collection=kwargs["_collection"],
-            inventory=kwargs["inventory"],
-            operation_history=kwargs["operation_history"],
             station_id=kwargs.get("station_id", None))
