30c58368b20b96b1754e883eaba9fdf13a337cb6
acedwards7 <adamcedwards1@gmail.com>
2024-08-23 14:18:09 +0100

    added comments to explain what is going on

--- tests/system_test/ikaStation.py
+++ tests/system_test/ikaStation.py
@@ -4,11 +4,14 @@ from ika_plate.handler import IkaPlate as backendIkaPlate
 
 
 class myStation(Communicator):
+    # setup the station
     def setup(self):
+        # create a device
         myika = backendIkaPlate(name="amyIka", station_id=self.station_id)
+        # add the device to the stations devices list
         self.devices.append(myika)
 
-
+#create station
 myStationInstance = myStation(lab_ip="localhost:8001")
 
 myStationInstance.run()
