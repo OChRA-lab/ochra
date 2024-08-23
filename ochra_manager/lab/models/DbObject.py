e6c940c76016109e3b11675a81da9ff93c5f12a5
acedwards7 <adamcedwards1@gmail.com>
2024-08-23 14:17:25 +0100

    renamed to dbobject

--- ochra_manager/lab/models/DbObject.py
+++ ochra_manager/lab/models/DbObject.py
@@ -6,6 +6,14 @@ class DbObject(DataModel):
     _collection: str = None
 
     def get_property(self, property_name):
+        """Gets a property from the database using the db_data
+
+        Args:
+            property_name (str): property to get
+
+        Returns:
+            Any: property value
+        """
         self.db_conn: DbConnection = DbConnection()
         return self.db_conn.read(self.db_data,
                                  property=property_name)
