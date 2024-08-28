fa6e62127bd163bfd21b703d8f261fcd4c1b2490
bt-nav <bjtnav@gmail.com>
2024-08-28 17:00:19 +0100

    added some comments

--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -7,26 +7,30 @@ from ochra_common.connections.db_connection import DbConnection
 class TestDocument(Document):
     name = StringField(required=True)
     
-    
 @pytest.fixture(scope="module")
 def db_connection():
+    # Create and connect to local test database
     db_conn = DbConnection(hostname = "localhost", db_name = "test_db")
     yield db_conn.db_adapter
+    # Cleanup and disconnect from test database
     db_conn.db_adapter.delete_database()
     disconnect(alias="test_db")
     
 def test_create(db_connection):
+    # Test creating a document
     doc = TestDocument(name="test_create_doc")
     inserted_id = db_connection.create({"collection_name": "test_collection"}, document=doc)
     assert isinstance(inserted_id, ObjectId), "The document was not created properly."
     
 def test_read(db_connection):
+    # Test reading a document
     doc = TestDocument(name="test_read_doc")
     inserted_id = db_connection.create({"collection_name": "test_collection"}, document=doc)
     read_doc = db_connection.read({"collection_name": "test_collection", "id": str(inserted_id)}, "name")
     assert read_doc == "test_read_doc", "The document was not read properly."
     
 def test_delete(db_connection):
+    # Test deleting a document
     doc = TestDocument(name="test_delete_doc")
     inserted_id = db_connection.create({"collection_name": "test_collection"}, document=doc)
     db_connection.delete("test_collection", {"_id": inserted_id})
