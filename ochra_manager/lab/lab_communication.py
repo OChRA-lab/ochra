e12884e536d3539b34acf1956b683c847a311c1b
acedwards7 <adamcedwards1@gmail.com>
2024-09-02 15:41:56 +0100

    removed old router things

--- ochra_manager/lab/lab_communication.py
+++ ochra_manager/lab/lab_communication.py
@@ -14,59 +14,9 @@ class LabCommunication():
     def __init__(self, host: str, port: int) -> None:
         self.host = host
         self.port = port
-        super().__init__()
         self.app = FastAPI()
-        self.router = APIRouter()
 
-        self.router.add_api_route(
-            "/object/set/{object_id}",
-            self.patch_object,
-            methods=["PATCH"])
-
-        self.router.add_api_route(
-            "/object/construct",
-            self.construct_object,
-            methods=["POST"])
-
-        self.router.add_api_route(
-            "/object/call/{object_id}",
-            self.call_on_object,
-            methods=["POST"])
-
-        self.router.add_api_route(
-            "/object/get/{objectName}",
-            self.get_object,
-            methods=["GET"])
-        self.router.add_api_route(
-            "/station/create",
-            self.create_station,
-            methods=["POST"])
-
-        self.router.add_api_route(
-            "/object/get_property/{id}/{property}",
-            self.get_object_property,
-            methods=["GET"])
-
-        self.app.include_router(self.router)
         self.app.include_router(device_router)
-
-    def patch_object(self, object_id: str, args: ObjectSet):
-        return lab_service.patch_object(object_id, args)
-
-    def construct_object(self, args: ObjectConstructionModel):
-        return lab_service.construct_object(args)
-
-    def call_on_object(self, object_id: str, args: ObjectCallModel):
-        return lab_service.call_on_object(object_id, args)
-
-    def get_object(self, objectName: str):
-        return lab_service.get_object(objectName)
-
-    def get_object_property(self, id: str, property: str):
-        return lab_service.get_object_property(id, property)
-
-    def create_station(self, request: Request):
-        return lab_service.create_station(request)
 
     def run(self):
         logger.info("started server")
