717896a98bc6982b051cfa0ca679f7a3cbd1ccb4
acedwards7 <adamcedwards1@gmail.com>
2024-09-02 15:14:46 +0100

    treat lab_communicator as a service

--- ochra_manager/lab/lab_communication.py
+++ ochra_manager/lab/lab_communication.py
@@ -1,14 +1,15 @@
 from ochra_common.connections.db_connection import DbConnection
 from fastapi import FastAPI, APIRouter, HTTPException, Request
+from .models.lab_request_models import ObjectSet, ObjectConstructionModel, ObjectCallModel
 import uvicorn
 import logging
 
-from .lab_processor import LabProcessor
+import lab_processor as lab_service
 
 logger = logging.getLogger(__name__)
 
 
-class LabCommunication(LabProcessor):
+class LabCommunication():
     def __init__(self, host: str, port: int) -> None:
         self.host = host
         self.port = port
@@ -45,6 +46,24 @@ class LabCommunication(LabProcessor):
             methods=["GET"])
 
         self.app.include_router(self.router)
+
+    def patch_object(self, object_id: str, args: ObjectSet):
+        return lab_service.patch_object(object_id, args)
+
+    def construct_object(self, args: ObjectConstructionModel):
+        return lab_service.construct_object(args)
+
+    def call_on_object(self, object_id: str, args: ObjectCallModel):
+        return lab_service.call_on_object(object_id, args)
+
+    def get_object(self, objectName: str):
+        return lab_service.get_object(objectName)
+
+    def get_object_property(self, id: str, property: str):
+        return lab_service.get_object_property(id, property)
+
+    def create_station(self, request: Request):
+        return lab_service.create_station(request)
 
     def run(self):
         logger.info("started server")
