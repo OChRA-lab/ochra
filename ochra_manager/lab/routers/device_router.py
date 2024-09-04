82dc1c135fe419640595d9e96f0e1db89f210888
acedwards7 <adamcedwards1@gmail.com>
2024-09-04 14:11:51 +0100

    updated to use new requests

--- ochra_manager/lab/routers/device_router.py
+++ ochra_manager/lab/routers/device_router.py
@@ -1,35 +1,35 @@
 import logging
 from fastapi import APIRouter
-from ..models.lab_request_models import ObjectSet, ObjectConstructionModel, ObjectCallModel
+from ..models.lab_api_models import ObjectCallRequest, ObjectCallResponse, ObjectPropertySetRequest, ObjectConstructionRequest, ObjectQueryResponse
 from ..lab_processor import lab_service
+from typing import Any
 
 logger = logging.getLogger(__name__)
 COLLECTION = "devices"
+
+
 class DeviceRouter(APIRouter):
     def __init__(self):
         prefix = f"/{COLLECTION}"
         super().__init__(prefix=prefix)
-        
-        self.post("/{station_id}/construct")(self.construct_device)
+
+        self.put("/{station_id}/construct")(self.construct_device)
         self.get("/{object_id}/get_property/{property}")(self.get_device_property)
         self.patch("/{object_id}/modify_property")(self.modify_device_property)
         self.post("/{station_id}/{object_id}/call_method")(self.call_device)
         self.get("/{station_id}/get")(self.get_device)
 
-    async def construct_device(self, station_id: str, args: ObjectConstructionModel):
-        db_entry = args.constructor_params
-        db_entry["station_id"] = station_id
-        db_entry["_type"] = args.object_type
-        return lab_service.construct_object(db_entry, COLLECTION)
+    async def construct_device(self, args: ObjectConstructionRequest):
+        return lab_service.construct_object(args, COLLECTION)
 
     async def get_device_property(self, object_id: str, property: str):
         return lab_service.get_object_property(object_id, COLLECTION, property)
 
-    async def modify_device_property(self, object_id: str, args: ObjectSet):
+    async def modify_device_property(self, object_id: str, args: ObjectPropertySetRequest):
         return lab_service.patch_object(object_id, COLLECTION, args)
 
-    async def call_device(self, object_id: str, args: ObjectCallModel):
+    async def call_device(self, object_id: str, args: ObjectCallRequest) -> ObjectCallResponse:
         return lab_service.call_on_object(object_id, COLLECTION, args)
 
-    async def get_device(self, station_id: str, device_name: str):
+    async def get_device(self, station_id: str, device_name: str) -> Any | ObjectQueryResponse:
         return lab_service.get_device(station_id, device_name)
