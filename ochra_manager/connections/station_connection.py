8a1836cca779987134369b3828c3f26153244f21
acedwards7 <adamcedwards1@gmail.com>
2024-09-04 16:32:43 +0100

    moved station connection

---
+++ ochra_manager/connections/station_connection.py
@@ -0,0 +1,20 @@
+from ochra_common.utils.singleton_meta import SingletonMeta
+from ochra_common.connections.rest_adapter import RestAdapter, Result, LabEngineException
+import logging
+
+
+class StationConnection():
+    def __init__(
+        self,
+        hostname: str = "10.24.57.154:8000",
+        api_key: str = "",
+        ssl_verify: bool = False,
+        logger: logging.Logger = None,
+    ):
+        self.rest_adapter: RestAdapter = RestAdapter(hostname, api_key, ssl_verify, logger)
+
+    def execute_op(self, op, deviceName, **kwargs):
+        data = {"operation": op,
+                "deviceName": deviceName,
+                "args": kwargs}
+        return self.rest_adapter.post(endpoint="process_op", data=data)
