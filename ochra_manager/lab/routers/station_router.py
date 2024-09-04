27fd9a1350eb5963c7adb070a36ed94c0d25c046
acedwards7 <adamcedwards1@gmail.com>
2024-09-04 15:21:06 +0100

    updated to use new models

--- ochra_manager/lab/routers/station_router.py
+++ ochra_manager/lab/routers/station_router.py
@@ -1,6 +1,6 @@
 import logging
 from fastapi import APIRouter
-from ..models.lab_request_models import ObjectSet, ObjectCallModel
+from ..models.lab_api_models import ObjectCallRequest, ObjectConstructionRequest, ObjectPropertySetRequest
 from ..lab_processor import lab_service
 
 logger = logging.getLogger(__name__)
@@ -11,24 +11,23 @@ class StationRouter(APIRouter):
     def __init__(self):
         super().__init__(prefix=f"/{COLLECTION}")
 
-        self.post("/construct")(self.construct_station)
+        self.put("/construct")(self.construct_station)
         self.get(
             "/{object_id}/get_property/{property}")(self.get_station_property)
         self.patch("/{object_id}/modify_property")(self.modify_property)
         self.post("/{object_id}/call_method")(self.call_method)
         self.get("/get")(self.get_station)
 
-    async def construct_station(self, name: str, constructor_params: dict):
-        db_entry = {"name": name, **constructor_params}
-        return lab_service.construct_object(db_entry, COLLECTION)
+    async def construct_station(self, args: ObjectConstructionRequest):
+        return lab_service.construct_object(args, COLLECTION)
 
     async def get_station_property(self, object_id: str, property: str):
         return lab_service.get_object_property(object_id, COLLECTION, property)
 
-    async def modify_property(self, object_id: str, args: ObjectSet):
+    async def modify_property(self, object_id: str, args: ObjectPropertySetRequest):
         return lab_service.patch_object(object_id, COLLECTION, args)
 
-    async def call_method(self, object_id: str, args: ObjectCallModel):
+    async def call_method(self, object_id: str, args: ObjectCallRequest):
         return lab_service.call_on_object(object_id, COLLECTION, args)
 
     async def get_station(self, station_name: str):
