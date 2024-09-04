a3f5842da2db1c14bb2c23974fca440cd09db87c
acedwards7 <adamcedwards1@gmail.com>
2024-09-04 15:17:18 +0100

    use instanced lab_service

--- ochra_manager/lab/routers/device_router.py
+++ ochra_manager/lab/routers/device_router.py
@@ -12,7 +12,7 @@ class DeviceRouter(APIRouter):
     def __init__(self):
         prefix = f"/{COLLECTION}"
         super().__init__(prefix=prefix)
-
+        self.lab_service = lab_service()
         self.put("/{station_id}/construct")(self.construct_device)
         self.get("/{object_id}/get_property/{property}")(self.get_device_property)
         self.patch("/{object_id}/modify_property")(self.modify_device_property)
@@ -20,16 +20,16 @@ class DeviceRouter(APIRouter):
         self.get("/{station_id}/get")(self.get_device)
 
     async def construct_device(self, args: ObjectConstructionRequest):
-        return lab_service.construct_object(args, COLLECTION)
+        return self.lab_service.construct_object(args, COLLECTION)
 
     async def get_device_property(self, object_id: str, property: str):
-        return lab_service.get_object_property(object_id, COLLECTION, property)
+        return self.lab_service.get_object_property(object_id, COLLECTION, property)
 
     async def modify_device_property(self, object_id: str, args: ObjectPropertySetRequest):
-        return lab_service.patch_object(object_id, COLLECTION, args)
+        return self.lab_service.patch_object(object_id, COLLECTION, args)
 
     async def call_device(self, object_id: str, args: ObjectCallRequest):
-        return lab_service.call_on_object(object_id, COLLECTION, args)
+        return self.lab_service.call_on_object(object_id, COLLECTION, args)
 
     async def get_device(self, station_id: str, device_name: str):
-        return lab_service.get_device(station_id, device_name)
+        return self.lab_service.get_device(station_id, device_name)
