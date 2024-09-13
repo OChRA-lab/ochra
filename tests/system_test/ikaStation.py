d304fcc43d7a29a7be72e1ab7ed4dd6651870b8d
acedwards7 <adamcedwards1@gmail.com>
2024-09-13 15:05:40 +0100

    Tests work

--- tests/system_test/ikaStation.py
+++ tests/system_test/ikaStation.py
@@ -8,7 +8,7 @@ class myStation(Communicator,WorkStation):
     # setup the station
     def setup(self):
         # create a device
-        myika = backendIkaPlate(name="amyIka", station_id=self.station_id)
+        myika = backendIkaPlate(name="amyIka", station_id=self._station_id)
         # add the device to the stations devices list
         self.devices.append(myika)
 
--- tests/system_test/ika_plate/abstract.py
+++ tests/system_test/ika_plate/abstract.py
@@ -2,7 +2,6 @@ from ochra_common.storage.device_inventory import DeviceInventory
 from ochra_common.equipment.device import Device
 from ochra_common.equipment.temperature_control import TemperatureControls
 from ochra_common.equipment.stir_control import StirControls
-from dataclasses import dataclass
 from abc import ABC
 
 
@@ -10,7 +9,6 @@ class IkaPlateInventory(DeviceInventory):
     pass
 
 
-@dataclass
 class IkaPlateAbstract(Device, TemperatureControls, StirControls, ABC):
     temperature: float = 0
     stir_speed: float = 0
