d6de989f2bf60cefa9fc274fdcb71b667bc48911
acedwards7 <adamcedwards1@gmail.com>
2024-09-16 14:51:55 +0100

    added get_by_station and get by itself

--- ochra_manager/lab/routers/device_router.py
+++ ochra_manager/lab/routers/device_router.py
@@ -17,10 +17,11 @@ class DeviceRouter(APIRouter):
         self.get("/{object_id}/get_property/{property}")(self.get_device_property)
         self.patch("/{object_id}/modify_property")(self.modify_device_property)
         self.post("/{station_id}/{object_id}/call_method")(self.call_device)
-        self.get("/{station_id}/get")(self.get_device)
+        self.get("/{station_id}/get_by_station")(self.get_device_by_station)
+        self.get("/get")(self.get_device)
 
     async def construct_device(self, args: ObjectConstructionRequest):
-        #TODO: we need to assign the object to the station somehow
+        # TODO: we need to assign the object to the station somehow
         return self.lab_service.construct_object(args, COLLECTION)
 
     async def get_device_property(self, object_id: str, property: str):
@@ -32,5 +33,8 @@ class DeviceRouter(APIRouter):
     async def call_device(self, object_id: str, args: ObjectCallRequest):
         return self.lab_service.call_on_object(object_id, COLLECTION, args)
 
-    async def get_device(self, station_id: str, device_name: str):
+    async def get_device_by_station(self, station_id: str, device_name: str):
         return self.lab_service.get_device(station_id, device_name)
+
+    async def get_device(self, name: str):
+        return self.lab_service.get_object_by_name(name,COLLECTION)
