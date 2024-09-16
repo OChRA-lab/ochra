49377e90b0f3999c8b000d4359608904cef88147
acedwards7 <adamcedwards1@gmail.com>
2024-09-16 14:52:27 +0100

    formatting

--- tests/system_test/frontendUser.py
+++ tests/system_test/frontendUser.py
@@ -1,22 +1,22 @@
 from ochra_common.connections.lab_connection import LabConnection
-from ika_plate.device import IkaPlate as frontendIkaPlate
+from ika_plate.device import IkaPlate
 
-#connect to lab
+# connect to lab
 mylabConnection = LabConnection("localhost:8001")
 
-#get my ika plate
-myIkaplate = frontendIkaPlate(name="amyIka")
+# get my ika plate
+myIkaplate = IkaPlate(name="amyIka")
 
-#try to set (frontend)
+# try to set (frontend)
 myIkaplate.status = "idle"
 
-#get status
+# get status
 print(myIkaplate.status)
 
 # get temperature to verify it changes
 print(myIkaplate.temperature)
 
-#try to set temperature with method
+# try to set temperature with method
 print(myIkaplate.set_temperature(temperature=100))
 
 # get temperature to verify it has changed
