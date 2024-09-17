aef8f73de44d7f5df7d10349d5ae74006797b8e4
acedwards7 <adamcedwards1@gmail.com>
2024-09-17 16:02:57 +0100

    updating tests

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -13,13 +13,39 @@ class TestDataModel(BaseModel, RestProxyMixin):
 def test_rest_proxy_mixin(MockLabConnection):
     mock_lab_conn = MockLabConnection.return_value
     mock_lab_conn.get_property.return_value = 'mock_value'
-
-    test_model = TestDataModel(cls="test_cls", params={"param": "value"})
     
+    test_model = TestDataModel(cls="test_cls", params={"param": "value"})
+    id = test_model.id
+
     test_model._mixin_hook("test_type", test_model.id)
 
+    #test getter
     assert test_model.cls == 'mock_value'
-    mock_lab_conn.get_property.assert_called_once_with('test_type', test_model._lab_conn.get_object_by_name.return_value.id, 'cls')
+    mock_lab_conn.get_property.assert_called_with('test_type', id, 'cls')
 
-def test_rest_proxy_mixin_read_only():
-    pass
+    #test setter
+    test_model.cls = 'new_value'
+    mock_lab_conn.set_property.assert_called_with('test_type', id, 'cls', 'new_value')
+
+class TestDataModelReadOnly(BaseModel, RestProxyMixinReadOnly):
+    id: UUID = Field(default_factory=uuid4)
+    cls: str = Field(default=None)
+    params: dict = Field(default=None)
+
+@patch("ochra_common.utils.mixins.LabConnection")
+def test_rest_proxy_mixin_read_only(MockLabConnection):
+    mock_lab_conn = MockLabConnection.return_value
+    mock_lab_conn.get_property.return_value = 'mock_value'
+    
+    test_model = TestDataModelReadOnly(cls="test_cls", params={"param": "value"})
+    id = test_model.id
+
+    test_model._mixin_hook("test_type", "myName")
+
+    #test getter
+    assert test_model.cls == 'mock_value'
+    mock_lab_conn.get_property.assert_called_with('test_type', id, 'cls')
+
+    #test setter
+    test_model.cls = 'new_value'
+    assert test_model.cls == 'mock_value'
