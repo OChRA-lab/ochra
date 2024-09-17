b584ce80ebdbfb60874f19308244ed55ff7d7cd9
acedwards7 <adamcedwards1@gmail.com>
2024-09-17 10:30:58 +0100

    update mixins to remove repeated instancing of lab conn

--- ochra_common/utils/mixins.py
+++ ochra_common/utils/mixins.py
@@ -5,23 +5,22 @@ from ..connections.lab_connection import LabConnection
 class RestProxyMixin:
 
     def _mixin_hook(self, endpoint: str, id: UUID) -> None:
+        self._lab_conn = LabConnection()
         for field in self.model_fields.keys():
 
             def getter(self, name=field):
-                lab_conn: LabConnection = LabConnection()
-                return lab_conn.get_property(endpoint, id, name)
+                return self.lab_conn.get_property(endpoint, id, name)
 
             def setter(self, value, name=field):
-                lab_conn: LabConnection = LabConnection()
                 print(f"set {name} to {value}")
-                return lab_conn.set_property(endpoint, id, name, value)
+                return self.lab_conn.set_property(endpoint, id, name, value)
 
             # Set the property on the class with the custom getter and setter
             setattr(self.__class__, field, property(getter, setter))
 
     def _lab_init(self, endpoint: str) -> None:
-        lab_conn: LabConnection = LabConnection()
-        lab_conn.construct_object(endpoint, self)
+        self._lab_conn = LabConnection()
+        self._lab_conn.construct_object(endpoint, self)
 
 
 class RestProxyMixinReadOnly:
@@ -31,8 +30,7 @@ class RestProxyMixinReadOnly:
         for field in self.model_fields.keys():
 
             def getter(self, name=field):
-                lab_conn: LabConnection = LabConnection()
-                return lab_conn.get_property(endpoint, id, name)
+                return self.lab_conn.get_property(endpoint, id, name)
 
             def setter(self, value, name=field):
                 print("Read Only")
@@ -41,5 +39,5 @@ class RestProxyMixinReadOnly:
             setattr(self.__class__, field, property(getter, setter))
 
     def _lab_init(self, endpoint: str, name: str) -> None:
-        lab_conn: LabConnection = LabConnection()
-        return lab_conn.get_object_by_name(endpoint, name).id
+        self._lab_conn: LabConnection = LabConnection()
+        return self._lab_conn.get_object_by_name(endpoint, name).id
