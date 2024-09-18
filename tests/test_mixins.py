825ea0f3f953dc734e97b9240457ee25b1af0a42
acedwards7 <adamcedwards1@gmail.com>
2024-09-18 09:45:21 +0100

    formatting

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -4,50 +4,55 @@ from uuid import UUID, uuid4
 from pydantic import BaseModel, Field
 from ochra_common.utils.mixins import RestProxyMixin, RestProxyMixinReadOnly
 
+
 class TestDataModel(BaseModel, RestProxyMixin):
     id: UUID = Field(default_factory=uuid4)
     cls: str = Field(default=None)
     params: dict = Field(default=None)
 
+
 @patch("ochra_common.utils.mixins.LabConnection")
 def test_rest_proxy_mixin(MockLabConnection):
     mock_lab_conn = MockLabConnection.return_value
     mock_lab_conn.get_property.return_value = 'mock_value'
-    
+
     test_model = TestDataModel(cls="test_cls", params={"param": "value"})
     id = test_model.id
 
     test_model._mixin_hook("test_type", test_model.id)
 
-    #test getter
+    # test getter
     assert test_model.cls == 'mock_value'
     mock_lab_conn.get_property.assert_called_with('test_type', id, 'cls')
 
-    #test setter
+    # test setter
     test_model.cls = 'new_value'
-    mock_lab_conn.set_property.assert_called_with('test_type', id, 'cls', 'new_value')
+    mock_lab_conn.set_property.assert_called_with(
+        'test_type', id, 'cls', 'new_value')
+
 
 class TestDataModelReadOnly(BaseModel, RestProxyMixinReadOnly):
     id: UUID = Field(default_factory=uuid4)
     cls: str = Field(default=None)
     params: dict = Field(default=None)
 
+
 @patch("ochra_common.utils.mixins.LabConnection")
 def test_rest_proxy_mixin_read_only(MockLabConnection):
     mock_lab_conn = MockLabConnection.return_value
     mock_lab_conn.get_property.return_value = 'mock_value'
-    
-    
-    test_model = TestDataModelReadOnly(cls="test_cls", params={"param": "value"})
+
+    test_model = TestDataModelReadOnly(
+        cls="test_cls", params={"param": "value"})
     id = test_model.id
     mock_lab_conn.get_object_by_name.return_value.id = id
-    
+
     test_model._mixin_hook("test_type", "myName")
 
-    #test getter
+    # test getter
     assert test_model.cls == 'mock_value'
     mock_lab_conn.get_property.assert_called_with('test_type', id, 'cls')
 
-    #test setter
+    # test setter
     test_model.cls = 'new_value'
     assert test_model.cls == 'mock_value'
