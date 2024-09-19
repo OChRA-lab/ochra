2b76319466d4bdea302b81bfb8765230d2f39c69
acedwards7 <adamcedwards1@gmail.com>
2024-09-19 14:35:43 +0100

    devices property already existed, referencing it

--- ochra_manager/Station/station.py
+++ ochra_manager/Station/station.py
@@ -6,11 +6,9 @@ from ochra_common.utils.mixins import RestProxyMixin
 class Station(Communicator, WorkStation, RestProxyMixin):
     def __init__(self, name, location, offline=False):
         super().__init__(name=name, location=location)
-        self.devices = []
         self._offline = offline
-        
 
     def add_device(self, device):
-        devices = self.devices
+        devices = self._devices
         devices.append(device)
-        self.devices = devices
+        self._devices = devices
