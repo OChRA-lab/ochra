5ff4eb3af990623c09dfdbb1743240363089f8f1
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2024-09-26 08:29:26 +0100

    modified mixins and their tests

--- ochra_common/utils/mixins.py
+++ ochra_common/utils/mixins.py
@@ -4,41 +4,42 @@ from ..connections.lab_connection import LabConnection
 
 class RestProxyMixin:
 
-    def _mixin_hook(self, endpoint: str, id: UUID) -> None:
+    def _mixin_hook(self, endpoint: str, object_id: UUID) -> None:
+        # add lab connection and construct object on the endpoint
         self._lab_conn = LabConnection()
+        self._lab_conn.construct_object(endpoint, self)
+
+        # change the getter and setter for each field to work with endpoint
         for field in self.model_fields.keys():
 
             def getter(self, name=field):
-                return self._lab_conn.get_property(endpoint, id, name)
+                return self._lab_conn.get_property(endpoint, object_id, name)
 
             def setter(self, value, name=field):
                 print(f"set {name} to {value}")
-                return self._lab_conn.set_property(endpoint, id, name, value)
+                return self._lab_conn.set_property(endpoint, object_id, name, value)
 
             # Set the property on the class with the custom getter and setter
             setattr(self.__class__, field, property(getter, setter))
-
-    def _lab_init(self, endpoint: str) -> None:
-        self._lab_conn = LabConnection()
-        self._lab_conn.construct_object(endpoint, self)
 
 
 class RestProxyMixinReadOnly:
 
     def _mixin_hook(self, endpoint: str, name) -> None:
-        id = self._lab_init(endpoint,name)
+        # add lab connection and get object id from the endpoint
+        self._lab_conn = LabConnection()
+
+        # TODO add a check if the object is a device or something else
+        object_id = self._lab_conn.get_object(endpoint, name).id
+
+        # change the getter and setter for each field to work with endpoint
         for field in self.model_fields.keys():
 
             def getter(self, name=field):
-                lab_conn = LabConnection()
-                return lab_conn.get_property(endpoint, id, name)
+                return self._lab_conn.get_property(endpoint, object_id, name)
 
             def setter(self, value, name=field):
                 print("Read Only")
 
             # Set the property on the class with the custom getter and setter
             setattr(self.__class__, field, property(getter, setter))
-
-    def _lab_init(self, endpoint: str, name: str) -> None:
-        lab_conn: LabConnection = LabConnection()
-        return lab_conn.get_object(endpoint, name).id
--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -1,58 +1,73 @@
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 from uuid import UUID, uuid4
 from pydantic import BaseModel, Field
 from ochra_common.utils.mixins import RestProxyMixin, RestProxyMixinReadOnly
+from collections import namedtuple
 
 
-class TestDataModel(BaseModel, RestProxyMixin):
+class TestDataModel(BaseModel):
     id: UUID = Field(default_factory=uuid4)
-    cls: str = Field(default=None)
+    name: str = Field(default=None)
     params: dict = Field(default=None)
+
+
+class TestData(TestDataModel, RestProxyMixin):
+    def __init__(self, object_id, **params):
+        super().__init__(id=object_id, name=self.__class__.__name__, params=params)
+        self._mixin_hook("/test/endpoint", object_id)
+
+
+class TestDataReadOnly(TestDataModel, RestProxyMixinReadOnly):
+    def __init__(self, name, **params):
+        super().__init__(id=uuid4(), name=name, params=params)
+        self._mixin_hook("/test/endpoint", name)
 
 
 @patch("ochra_common.utils.mixins.LabConnection")
 def test_rest_proxy_mixin(MockLabConnection):
-    mock_lab_conn = MockLabConnection.return_value
-    mock_lab_conn.get_property.return_value = 'mock_value'
 
-    test_model = TestDataModel(cls="test_cls", params={"param": "value"})
-    id = test_model.id
+    object_id = uuid4()
+    test_model = TestData(object_id, params={"param": "value"})
 
-    test_model._mixin_hook("test_type", test_model.id)
+    # to get the instance of the used mock inside TestData
+    mock_lab_connection = MockLabConnection.return_value
+
+    # test construction
+    mock_lab_connection.construct_object.assert_called_with(
+        "/test/endpoint", test_model)
 
     # test getter
-    assert test_model.cls == 'mock_value'
-    mock_lab_conn.get_property.assert_called_with('test_type', id, 'cls')
+    test_model.name
+    mock_lab_connection.get_property.assert_called_with(
+        "/test/endpoint", object_id, 'name')
 
     # test setter
-    test_model.cls = 'new_value'
-    mock_lab_conn.set_property.assert_called_with(
-        'test_type', id, 'cls', 'new_value')
+    test_model.name = 'new_value'
+    mock_lab_connection.set_property.assert_called_with(
+        "/test/endpoint", object_id, 'name', 'new_value')
 
 
-class TestDataModelReadOnly(BaseModel, RestProxyMixinReadOnly):
-    id: UUID = Field(default_factory=uuid4)
-    cls: str = Field(default=None)
-    params: dict = Field(default=None)
-
-
-@patch("ochra_common.utils.mixins.LabConnection")
+@ patch("ochra_common.utils.mixins.LabConnection")
 def test_rest_proxy_mixin_read_only(MockLabConnection):
-    mock_lab_conn = MockLabConnection.return_value
-    mock_lab_conn.get_property.return_value = 'mock_value'
+    mock_lab_connection = MockLabConnection.return_value
+    Response = namedtuple('Response', ['id'])
+    res = Response(id=uuid4())
+    mock_lab_connection.get_object.return_value = res
 
-    test_model = TestDataModelReadOnly(
-        cls="test_cls", params={"param": "value"})
-    id = test_model.id
-    mock_lab_conn.get_object_by_name.return_value.id = id
+    name = "unique_name"
+    test_model = TestDataReadOnly(
+        name=name, params={"param": "value"})
 
-    test_model._mixin_hook("test_type", "myName")
+    # test object retrieval
+    mock_lab_connection.get_object.assert_called_with(
+        "/test/endpoint", name)
 
     # test getter
-    assert test_model.cls == 'mock_value'
-    mock_lab_conn.get_property.assert_called_with('test_type', id, 'cls')
+    test_model.name
+    mock_lab_connection.get_property.assert_called_with(
+        "/test/endpoint", res.id, 'name')
 
     # test setter
-    test_model.cls = 'new_value'
-    assert test_model.cls == 'mock_value'
+    test_model.name = 'new_value'
+    mock_lab_connection.set_property.assert_not_called()
