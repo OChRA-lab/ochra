57cb56e72b93e068d370ae883867e2ddedec5aed
acedwards7 <adamcedwards1@gmail.com>
2024-10-01 14:37:51 +0100

    read only load from id

--- ochra_common/utils/mixins.py
+++ ochra_common/utils/mixins.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 from ..connections.lab_connection import LabConnection
+import inspect
 
 
 class RestProxyMixin:
@@ -7,7 +8,8 @@ class RestProxyMixin:
     def _mixin_hook(self, endpoint: str, object_id: UUID) -> None:
         # add lab connection and construct object on the endpoint
         self._lab_conn = LabConnection()
-        self._lab_conn.construct_object(endpoint, self)
+        self._lab_conn.construct_object(
+            endpoint + "/" + str(self.station_id), self)
 
         # change the getter and setter for each field to work with endpoint
         for field in self.model_fields.keys():
@@ -27,19 +29,30 @@ class RestProxyMixinReadOnly:
 
     def _mixin_hook(self, endpoint: str, name) -> None:
         # add lab connection and get object id from the endpoint
-        self._lab_conn = LabConnection()
+        lab_conn = LabConnection()
 
         # TODO add a check if the object is a device or something else
-        object_id = self._lab_conn.get_object(endpoint, name).id
+        object_id = lab_conn.get_object(endpoint, name).id
 
         # change the getter and setter for each field to work with endpoint
         for field in self.model_fields.keys():
             if field not in ["id", "cls"]:
                 def getter(self, name=field):
-                    return self._lab_conn.get_property(endpoint, object_id, name)
+                    return lab_conn.get_property(endpoint, object_id, name)
 
                 def setter(self, value, name=field):
                     print("Read Only")
 
                 # Set the property on the class with the custom getter and setter
                 setattr(self.__class__, field, property(getter, setter))
+
+    @classmethod
+    def from_id(cls, endpoint: str, object_id: UUID):
+        lab_conn: LabConnection = LabConnection()
+        constructer_args = inspect.signature(cls)
+        args = {}
+        for arg in constructer_args.parameters:
+            arg_value = lab_conn.get_property(endpoint, object_id, arg)
+            args[arg] = arg_value
+
+        return cls(**args)
