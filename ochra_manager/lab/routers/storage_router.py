09d294f07072fbe982005de84eb55303634c63fc
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2024-10-15 15:01:09 +0100

    added storage router

---
+++ ochra_manager/lab/routers/storage_router.py
@@ -0,0 +1,40 @@
+import logging
+from fastapi import APIRouter
+from ochra_common.connections.api_models import ObjectPropertySetRequest, ObjectConstructionRequest
+from ..lab_service import LabService
+from ochra_common.utils.misc import is_valid_uuid
+
+logger = logging.getLogger(__name__)
+COLLECTIONS = ["consumables", "stocks", "containers", "inventories", "reagents"]
+
+
+class StorageRouter(APIRouter):
+    def __init__(self):
+        prefix = "/storage"
+        super().__init__(prefix=prefix)
+        self.lab_service = LabService()
+
+        # routes for containers
+        self.put("/{object_type}/construct")(self.construct_storage_item)
+        self.get("/{object_type}/{object_id}/get_property/{property}")(self.get_storage_item_property)
+        self.patch("/{object_type}/{object_id}/modify_property")(self.modify_storage_item_property)
+        self.get("/{object_type}/get")(self.get_storage_item)
+
+    async def construct_storage_item(self, object_type: str, args: ObjectConstructionRequest):
+        collection = object_type if object_type in COLLECTIONS else None
+        return self.lab_service.construct_object(args, collection)
+
+    async def get_storage_item_property(self, object_type: str, object_id: str, property: str):
+        collection = object_type if object_type in COLLECTIONS else None
+        return self.lab_service.get_object_property(object_id, collection, property)
+
+    async def modify_storage_item_property(self, object_type: str, object_id: str, args: ObjectPropertySetRequest):
+        collection = object_type if object_type in COLLECTIONS else None
+        return self.lab_service.patch_object(object_id, collection, args)
+
+    async def get_storage_item(self, object_type: str, identifier: str):
+        collection = object_type if object_type in COLLECTIONS else None
+        if is_valid_uuid(identifier):
+            return self.lab_service.get_object_by_id(identifier, collection)
+        else:
+            return self.lab_service.get_object_by_name(identifier, collection)
