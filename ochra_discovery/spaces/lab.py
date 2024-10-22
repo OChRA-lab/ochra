33ae3973166c416c6568658ef3b2274dbf0db62a
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2024-10-22 16:51:48 +0100

    changed lab return type hint. work_station returns device

--- ochra_discovery/spaces/lab.py
+++ ochra_discovery/spaces/lab.py
@@ -1,18 +1,19 @@
 from ochra_common.spaces.lab import Lab
-from ochra_common.spaces.station import Station
+from ochra_common.spaces.work_station import WorkStation
+from ochra_common.spaces.storage_station import StorageStation
 from ochra_common.agents.robot import Robot
 from ochra_common.connections.lab_connection import LabConnection
-from typing import List, Type
+from typing import List, Type, Union
 
 
 class Lab(Lab):
     def __init__(self, hostname: str):
         self._lab_conn: LabConnection = LabConnection(hostname)
 
-    def get_station(self, station_name: str) -> Type[Station]:
+    def get_station(self, station_name: str) -> Union[WorkStation, StorageStation]:
         return self._lab_conn.get_object("lab/stations", station_name)
 
-    def get_stations(self) -> List[Type[Station]]:
+    def get_stations(self) -> List[Union[WorkStation, StorageStation]]:
         return self._lab_conn.get_all_objects("lab/stations")
 
     def get_robot(self, robot_name: str) -> Type[Robot]:
--- ochra_discovery/spaces/work_station.py
+++ ochra_discovery/spaces/work_station.py
@@ -1,8 +1,13 @@
+from ochra_common.equipment.device import Device
 from ochra_common.spaces.work_station import WorkStation
 from ochra_common.utils.mixins import RestProxyMixinReadOnly
 from uuid import UUID
+from typing import Type, Union
 
 class WorkStation(WorkStation, RestProxyMixinReadOnly):
     def __init__(self, object_id: UUID):
         super().__init__()
         self._mixin_hook(self._endpoint, object_id)
+
+    def get_device(self, device_identifier: Union[str, UUID]) -> Type[Device]:
+        return self._lab_conn.get_object("devices", device_identifier)
