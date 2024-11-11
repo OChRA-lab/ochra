40d20d1a1e45f0bfb676b6033b214331af810e25
qijie <qijie44@gmail.com>
2024-11-11 13:36:27 +0000

    extended the operation_result DataModel

--- ochra_common/equipment/operation_result.py
+++ ochra_common/equipment/operation_result.py
@@ -9,18 +9,19 @@ class OperationResult(DataModel):
     Abstract result class to keep results formatted and structured.
 
     Attributes:
-        data_entry_id (uuid.UUID): The unique identifier of the data entry
         success (bool): The outcome of the operation. 
         error (str): The error if the operation failed. Defaulted to None 
         data (Any): Data of the result. Can be any data
-        data_file_type (str): The file type of the result. Defaulted to None, which just means the data is saved as raw text
+        data_file_type (str): The file type of the result. Leave as None if the data_type is defined below
+        data_type (str): the python data model () 
     """
-
-    data_entry_id: UUID
     success: bool
-    error: str = Field(default=None)
-    data: Any
-    data_file_type: str = Field(default=None)
+    error: str = Field(default="")
+    data: Any = Field(default=None)
+    data_file_type: str = Field(default="")
+    data_type: str = Field(default="")
+
+    _endpoint = "operation_results"  # associated endpoint for all operations
 
     def put_data(self) -> bool:
         """
