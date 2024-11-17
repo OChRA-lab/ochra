962d471b547959c0746ab13861ca2655a029dfe8
qijie <qijie44@gmail.com>
2024-11-17 03:03:51 +0000

    added get_data method to OperationResult

--- ochra_discovery/equipment/operation_result.py
+++ ochra_discovery/equipment/operation_result.py
@@ -9,4 +9,17 @@ class OperationResult(OperationResult, RestProxyMixinReadOnly):
         self._mixin_hook(self._endpoint, id)
 
     def get_data(self, filename:str = None) -> bool:
-        pass
+        """Gets the data from the server and saves it to the filename. If filename is not provided, saves it as the original filename
+        
+        Args:
+            filename (str): The name the file is to be saved. Requires file extension to be part of the name
+
+        Returns:
+            bool: True if successful
+        """
+        data = self._lab_conn.get_data("operation_results", self.id)
+        if filename == None:
+            filename = self._lab_conn.get_property("operation_results", self.id, "data_file_name")
+        with open(filename, "wb") as file:
+            file.write(data)
+            return True
