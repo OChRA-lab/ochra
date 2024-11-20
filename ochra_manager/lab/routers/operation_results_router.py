97b37a0106196b8082c721c709987f5ad2df16fd
qijie <qijie44@gmail.com>
2024-11-20 12:52:56 +0000

    Method name changes in OperationResultsRouter construct_op -> construct_result get_op_property -> get_property modify_op_property -> modify_property get_op -> get_result

--- ochra_manager/lab/routers/operation_results_router.py
+++ ochra_manager/lab/routers/operation_results_router.py
@@ -25,17 +25,17 @@ class OperationResultRouter(APIRouter):
         prefix = f"/{COLLECTION}"
         super().__init__(prefix=prefix)
         self.lab_service = LabService()
-        self.put("/construct")(self.construct_op)
-        self.get("/{object_id}/get_property/{property}")(self.get_op_property)
-        self.patch("/{object_id}/modify_property")(self.modify_op_property)
-        self.get("/get")(self.get_op)
+        self.put("/construct")(self.construct_result)
+        self.get("/{object_id}/get_property/{property}")(self.get_property)
+        self.patch("/{object_id}/modify_property")(self.modify_property)
+        self.get("/get")(self.get_result)
         self.get("/{object_id}/get_data/")(self.get_data)
         self.patch("/{object_id}/put_data/")(self.put_data)
 
-    async def construct_op(self, args: ObjectConstructionRequest):
+    async def construct_result(self, args: ObjectConstructionRequest):
         return self.lab_service.construct_object(args, COLLECTION)
 
-    async def get_op_property(self, object_id: str, property: str):
+    async def get_property(self, object_id: str, property: str):
         if property == "data":
             # checking if there is a file in the data property
             if (
@@ -47,10 +47,10 @@ class OperationResultRouter(APIRouter):
             data = self.lab_service.get_object_property(object_id, COLLECTION, property)
         return data
 
-    async def modify_op_property(self, object_id: str, args: ObjectPropertySetRequest):
+    async def modify_property(self, object_id: str, args: ObjectPropertySetRequest):
         return self.lab_service.patch_object(object_id, COLLECTION, args)
 
-    async def get_op(self, identifier: str):
+    async def get_result(self, identifier: str):
         if is_valid_uuid(identifier):
             value = self.lab_service.get_object_by_id(identifier, COLLECTION)
         else:
