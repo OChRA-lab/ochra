34c4501c2d27c52a62d6409c14d1cc8e80f85f52
acedwards7 <adamcedwards1@gmail.com>
2024-11-21 10:50:19 +0000

    added doc strings for work station

--- ochra_discovery/spaces/work_station.py
+++ ochra_discovery/spaces/work_station.py
@@ -8,11 +8,32 @@ from typing import Type, Union
 
 class WorkStation(WorkStation, RestProxyMixinReadOnly):
     def __init__(self, object_id: UUID):
+        """Station object that provides access to the devices and robots.
+
+        Args:
+            object_id (UUID): UUID of Station
+        """
         super().__init__()
         self._mixin_hook(self._endpoint, object_id)
 
     def get_device(self, device_identifier: Union[str, UUID]) -> Type[Device]:
+        """Get a device object by name or UUID.
+
+        Args:
+            device_identifier (Union[str, UUID]): Name or UUID of the device to get.
+
+        Returns:
+            Type[Device]: The device object.
+        """
         return self._lab_conn.get_object("devices", device_identifier)
 
     def get_robot(self, robot_identifier: Union[str, UUID]) -> Type[Manipulator]:
+        """Get a robot object by name or UUID.
+
+        Args:
+            robot_identifier (Union[str, UUID]): Name or UUID of the robot to get.
+
+        Returns:
+            Type[Manipulator]: The robot object.
+        """
         return self._lab_conn.get_object("robots", robot_identifier)
