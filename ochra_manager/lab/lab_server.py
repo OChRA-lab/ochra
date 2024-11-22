927e7122aee2a860751f8ff941bc3a65ebaf2998
acedwards7 <adamcedwards1@gmail.com>
2024-11-22 11:05:52 +0000

    lab server docstrings

--- ochra_manager/lab/lab_server.py
+++ ochra_manager/lab/lab_server.py
@@ -15,6 +15,12 @@ logger = logging.getLogger(__name__)
 
 class LabServer:
     def __init__(self, host: str, port: int) -> None:
+        """Setup a lab server with the given host and port 
+
+        Args:
+            host (str): host ip address
+            port (int): port to open the server on
+        """
         self.host = host
         self.port = port
         self.app = FastAPI()
@@ -28,6 +34,11 @@ class LabServer:
         self.app.include_router(OperationResultRouter())
 
     def get_caller_variable_name(self):
+        """Find the name of the variable that called this function
+
+        Returns:
+            str: filename:variable_name.app
+        """
         frame = inspect.currentframe().f_back.f_back
         fileName: str = frame.f_locals.get("__file__")[:-3]
         fileNameSplit = fileName.split("\\")
@@ -38,6 +49,8 @@ class LabServer:
         return fileNameSplit[-1] + ":" + variableName + ".app"
 
     def run(self) -> None:
+        """launches the server on the initialized host and port
+        """
         logger.info("started server")
         app = self.get_caller_variable_name()
         uvicorn.run(app, host=self.host, port=self.port, workers=8)
