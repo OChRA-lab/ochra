fd324d81a6ecdd41b5d61d242cb79e401b52d216
acedwards7 <adamcedwards1@gmail.com>
2024-12-05 12:58:14 +0000

    formatting and type hints

--- ochra_manager/equipment/operation_result.py
+++ ochra_manager/equipment/operation_result.py
@@ -1,10 +1,20 @@
 from ochra_common.equipment.operation_result import OperationResult
 from ochra_common.utils.mixins import RestProxyMixin
 from uuid import UUID
+from typing import Any
+from enum import Enum
 
 
 class OperationResult(OperationResult, RestProxyMixin):
-    def __init__(self, success, error, result_data, data_file_name, data_type, data_status):
+    def __init__(
+        self,
+        success: bool,
+        error: str,
+        result_data: Any,
+        data_file_name: str,
+        data_type: str,
+        data_status: Enum,
+    ):
         """result class to keep results formatted and structured.
 
         Args:
