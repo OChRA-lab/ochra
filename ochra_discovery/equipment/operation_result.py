0b3715c249161be3aa6d6d69d600dab6d1df87f7
qijie <qijie44@gmail.com>
2024-12-10 11:39:33 +0000

    rename get_file to save_data to better reflect what it does Added unzip method to save_data for folders

--- ochra_discovery/equipment/operation_result.py
+++ ochra_discovery/equipment/operation_result.py
@@ -2,7 +2,8 @@ from ochra_common.equipment.operation_result import OperationResult
 from ochra_common.utils.mixins import RestProxyMixinReadOnly
 from uuid import UUID
 from typing import Union, Type
-
+import shutil
+from os import remove
 
 class OperationResult(OperationResult, RestProxyMixinReadOnly):
     def __init__(self, id: UUID):
@@ -14,27 +15,34 @@ class OperationResult(OperationResult, RestProxyMixinReadOnly):
         super().__init__()
         self._mixin_hook(self._endpoint, id)
 
-    def get_file(self, filename: str = None) -> bool:
-        """Gets the data from the server and saves it to the filename. If filename is not provided, saves it as the original filename
+    def save_data(self, name: str = None) -> bool:
+        """Gets the data from the server and saves it to name. If name is not provided, saves it as the original name
 
         Args:
-            filename (str): The name the file is to be saved. Does not require file extension to be part of the name
+            name (str): The name the file is to be saved. Does not require file extension to be part of the name
 
         Returns:
             bool: True if successful
         """
         data = self._lab_conn.get_data("operation_results", self.id)
-        if filename == None:
-            filename = self._lab_conn.get_property(
+        if name == None:
+            name = self._lab_conn.get_property(
                 "operation_results", self.id, "data_file_name"
             )
         else:
-            filename = (
-                filename + "."
+            name = (
+                name + "."
                 + self._lab_conn.get_property(
                     "operation_results", self.id, "data_file_name"
                 ).split(".")[-1]
             )
-        with open(filename, "wb") as file:
+        with open(name, "wb") as file:
             file.write(data)
-            return True
+        
+        if self._lab_conn.get_property(
+            "operation_results", self.id, "data_type"
+        ) == "folder":
+            shutil.unpack_archive(name, name.split(".")[0], "zip")
+            remove(name)
+
+        return True
