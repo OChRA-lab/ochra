a1ec28084c4b46ad8a214435fad185fab3ce8f5e
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2025-01-17 16:38:48 +0000

    changed mobile_robot go_to method to not use location

--- ochra_common/agents/mobile_platform.py
+++ ochra_common/agents/mobile_platform.py
@@ -1,7 +1,6 @@
 from pydantic import Field
 from typing import Dict, Any
 from .robot import Robot
-from ..spaces.location import Location
 
 
 class MobilePlatform(Robot):
@@ -14,12 +13,12 @@ class MobilePlatform(Robot):
 
     conditions: Dict[str, Any] = Field(default_factory=dict)
 
-    def go_to(self, location: Location) -> bool:
+    def go_to(self, args: Dict[str, Any]) -> bool:
         """
-        Move the mobile platform to a specified location.
+        Move the mobile platform to a specified location using given args.
 
         Args:
-            location (Location): The target location to move to.
+            args (Dict[str, Any]): Argument needed for robot navigation to the target location.
 
         Returns:
             bool: True if the platform successfully moved to the location
