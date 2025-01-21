7932476c94ba86b326365a63bf69a18c1c34a1b3
acedwards7 <adamcedwards1@gmail.com>
2025-01-21 13:43:51 +0000

    added PropertygetRequest

--- ochra_manager/lab/routers/operation_results_router.py
+++ ochra_manager/lab/routers/operation_results_router.py
@@ -11,6 +11,7 @@ from ochra_common.connections.api_models import (
     ObjectPropertySetRequest,
     ObjectConstructionRequest,
     ObjectQueryResponse,
+    ObjectPropertyGetRequest
 )
 from ..lab_service import LabService
 from ochra_common.utils.misc import is_valid_uuid
@@ -26,20 +27,20 @@ class OperationResultRouter(APIRouter):
         super().__init__(prefix=prefix)
         self.lab_service = LabService(folderpath)
         self.put("/construct")(self.construct_result)
-        self.get("/{object_id}/get_property/{property}")(self.get_property)
-        self.patch("/{object_id}/modify_property")(self.modify_property)
+        self.get("/{identifier}/get_property")(self.get_property)
+        self.patch("/{identifier}/modify_property")(self.modify_property)
         self.get("/get")(self.get_result)
-        self.get("/{object_id}/get_data/")(self.get_data)
-        self.patch("/{object_id}/put_data/")(self.put_data)
+        self.get("/{identifier}/get_data/")(self.get_data)
+        self.patch("/{identifier}/put_data/")(self.put_data)
 
     async def construct_result(self, args: ObjectConstructionRequest):
         return self.lab_service.construct_object(args, COLLECTION)
 
-    async def get_property(self, object_id: str, property: str):
-        return self.lab_service.get_object_property(object_id, COLLECTION, property)
+    async def get_property(self, identifier: str, args: ObjectPropertyGetRequest):
+        return self.lab_service.get_object_property(identifier, COLLECTION, args)
 
-    async def modify_property(self, object_id: str, args: ObjectPropertySetRequest):
-        return self.lab_service.patch_object(object_id, COLLECTION, args)
+    async def modify_property(self, identifier: str, args: ObjectPropertySetRequest):
+        return self.lab_service.patch_object(identifier, COLLECTION, args)
 
     async def get_result(self, identifier: str):
         if is_valid_uuid(identifier):
@@ -48,11 +49,11 @@ class OperationResultRouter(APIRouter):
             value = self.lab_service.get_object_by_name(identifier, COLLECTION)
         return value
 
-    async def get_data(self, object_id: str):
-        value = self.lab_service.get_file(object_id, COLLECTION)
+    async def get_data(self, identifier: str):
+        value = self.lab_service.get_file(identifier, COLLECTION)
         response = Response(value)
         return response
 
-    async def put_data(self, object_id: str, file: UploadFile = File(...)):
+    async def put_data(self, identifier: str, file: UploadFile = File(...)):
         result_data = await file.read()
-        return self.lab_service.patch_file(object_id, COLLECTION, result_data)
+        return self.lab_service.patch_file(identifier, COLLECTION, result_data)
