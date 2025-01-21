88ded258097a56bafc6c0ef818fe98ededdd8007
acedwards7 <adamcedwards1@gmail.com>
2025-01-21 13:32:33 +0000

    update RobotRouter

--- ochra_manager/lab/routers/robot_router.py
+++ ochra_manager/lab/routers/robot_router.py
@@ -4,6 +4,7 @@ from ochra_common.connections.api_models import (
     ObjectCallRequest,
     ObjectPropertySetRequest,
     ObjectConstructionRequest,
+    ObjectPropertyGetRequest,
 )
 from ..lab_service import LabService
 from ochra_common.utils.misc import is_valid_uuid
@@ -18,22 +19,22 @@ class RobotRouter(APIRouter):
         super().__init__(prefix=prefix)
         self.lab_service = LabService()
         self.put("/construct")(self.construct_robot)
-        self.get("/{object_id}/get_property/{property}")(self.get_property)
-        self.patch("/{object_id}/modify_property")(self.modify_property)
-        self.post("/{object_id}/call_method")(self.call_robot)
+        self.get("/{identifier}/get_property/{property}")(self.get_property)
+        self.patch("/{identifier}/modify_property")(self.modify_property)
+        self.post("/{identifier}/call_method")(self.call_robot)
         self.get("/get")(self.get_robot)
 
     async def construct_robot(self, args: ObjectConstructionRequest):
         return self.lab_service.construct_object(args, COLLECTION)
 
-    async def get_property(self, object_id: str, property: str):
-        return self.lab_service.get_object_property(object_id, COLLECTION, property)
+    async def get_property(self, identifier: str, args: ObjectPropertyGetRequest):
+        return self.lab_service.get_object_property(identifier, COLLECTION, args)
 
-    async def modify_property(self, object_id: str, args: ObjectPropertySetRequest):
-        return self.lab_service.patch_object(object_id, COLLECTION, args)
+    async def modify_property(self, identifier: str, args: ObjectPropertySetRequest):
+        return self.lab_service.patch_object(identifier, COLLECTION, args)
 
-    async def call_robot(self, object_id: str, args: ObjectCallRequest):
-        return self.lab_service.call_on_object(object_id, COLLECTION, args)
+    async def call_robot(self, identifier: str, args: ObjectCallRequest):
+        return self.lab_service.call_on_object(identifier, COLLECTION, args)
 
     async def get_robot(self, identifier: str):
         if is_valid_uuid(identifier):
