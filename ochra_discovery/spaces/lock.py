5338099142536c11cbdafbd908bd03c575424273
acedwards7 <adamcedwards1@gmail.com>
2025-01-24 14:34:09 +0000

    didnt need any of the lab connection stuff

--- ochra_discovery/spaces/lock.py
+++ ochra_discovery/spaces/lock.py
@@ -1,12 +1,10 @@
-from ochra_common.connections.lab_connection import LabConnection
 from ochra_common.spaces.station import Station
 
 class Lock(object):
     def __init__(self, station: Station):
-        self._lab_conn: LabConnection = LabConnection()
         self.station = station
-        self.station.lock(self._lab_conn._session_id)
+        self.station.lock()
     def __enter__(self):
         return self.station
     def __exit__(self, exc_type, exc_value, traceback):
-        self.station.unlock(self._lab_conn._session_id)
+        self.station.unlock()
