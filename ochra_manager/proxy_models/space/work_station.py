0e35395d1041eda2758949e9d13a120d16435863
sgalnunt <sgalnunt@liverpool.ac.uk>
2025-01-28 11:02:42 +0000

    added bug fix for work_station

--- ochra_manager/proxy_models/space/work_station.py
+++ ochra_manager/proxy_models/space/work_station.py
@@ -25,7 +25,7 @@ class WorkStation(WorkStation, RestProxyMixin):
         self._mixin_hook("stations", self.id)
 
     def add_device(self, device: Type[Device]):
-        device.station_id = self.id
+        device.owner_station = self.get_base_model()
         devices = self.devices
         devices.append(device.id)
         self.devices = devices
