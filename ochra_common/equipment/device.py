64c732a249730ad8f51f77d426057b267da78395
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2025-03-13 17:01:41 +0000

    modified device and station models

--- ochra_common/equipment/device.py
+++ ochra_common/equipment/device.py
@@ -15,14 +15,12 @@ class Device(DataModel):
         name (str): The name of the device.
         inventory (DeviceInventory): The inventory associated with the device.
         status (ActiveStatus): The current active status of the device. Defaults to IDLE.
-        operation_history (List[Operation]): A list of operations performed by the device.
         owner_station (str): ID of the station which the device belongs to.
     """
 
     name: str
     inventory: Inventory = Field(default=None)
     status: ActivityStatus = ActivityStatus.IDLE
-    operation_history: List[Operation] = Field(default_factory=list)
     owner_station: UUID = Field(default=None)
 
     _endpoint = "devices"  # associated endpoint for all devices
