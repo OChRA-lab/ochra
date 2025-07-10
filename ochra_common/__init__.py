cb69f5e8257c08eb299d9c0f3389f5cfa14ebf9d
btnav <bjtnav@gmail.com>
2025-07-10 15:23:18 +0100

    rename to _configure_ochra

--- ochra_common/__init__.py
+++ ochra_common/__init__.py
@@ -1,9 +1,9 @@
 from .utils.logging_config import configure_logging
 
 
-def _configure_framework():
+def _configure_ochra():
     """Initialise the OChRA framework."""
     configure_logging()
 
 
-_configure_framework()
+_configure_ochra()
