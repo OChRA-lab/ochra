7e8e1754a6f37d11b2d07329c6bbfc92769895ac
btnav <bjtnav@gmail.com>
2025-07-10 15:03:44 +0100

    get device module from call stack

--- ochra_common/utils/logging_config.py
+++ ochra_common/utils/logging_config.py
@@ -1,5 +1,7 @@
+import inspect
 import logging
 from pathlib import Path
+import re
 
 # Filter to allow INFO level only
 class InfoPassFilter(logging.Filter):
@@ -15,7 +17,20 @@ for parent in current_path.parents:
         WORKSPACE_ROOT = parent
 LOG_DIR = WORKSPACE_ROOT / "ochra_logs"
 
-_default_getLogger = logging.getLogger
+_default_getLogger = logging.getLogge()r
+
+def _get_device_module():
+    frame = inspect.currentframe()
+    try:
+        caller_frame = frame.f_back
+        while caller_frame:
+            module_name = caller_frame.f_globals.get("__name__")
+            if module_name and "handler" in module_name:
+                return module_name
+            caller_frame = caller_frame.f_back
+    finally:
+        del frame
+    return None
 
 def custom_getLogger(name=None):
 
