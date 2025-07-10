5d20207359935f31487c7f52958f8a072ef8bf42
btnav <bjtnav@gmail.com>
2025-07-10 14:55:52 +0100

    set log directory

--- ochra_common/utils/logging_config.py
+++ ochra_common/utils/logging_config.py
@@ -1,6 +1,16 @@
 import logging
+from pathlib import Path
 
 # Filter to allow INFO level only
 class InfoPassFilter(logging.Filter):
     def filter(self, record):
         return record.levelno == logging.INFO
+
+# Find OChRA root and set log directory
+current_path = Path(__file__).resolve()
+for parent in current_path.parents:
+    if ((parent / "ochra_common").exists() and (parent / "ochra_manager").exists()) or (
+        (parent / "ochra_common").exists() and (parent / "ochra_discovery").exists()
+    ):
+        WORKSPACE_ROOT = parent
+LOG_DIR = WORKSPACE_ROOT / "ochra_logs"
