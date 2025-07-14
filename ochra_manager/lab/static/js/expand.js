aa6dabe3d7f2cc6a9dbb7ae916b63fba2aa61d6d
Uche <buche@liverpool.ac.uk>
2025-07-14 16:25:16 +0100

    Started on the first part of changing the views - Station side panel

---
+++ ochra_manager/lab/static/js/expand.js
@@ -0,0 +1,20 @@
+var container = document.querySelector('#SIDEPANEL_CONTAINER');
+var toggleButton = document.querySelector('#TOGGLE_BUTTON');
+// When the user clicks the toggle button
+function toggleSidePanel() {
+    const isCollapsed = container.style.width === '2.45rem';
+
+    if (isCollapsed) {
+        container.style.width = '32rem';  // Expanded 103rem
+        toggleButton.textContent = '◀';
+    } else {
+        container.style.width = '2.45rem';  // Collapsed
+        toggleButton.textContent = '▶';
+    }
+}
+
+// Set initial state
+container.style.width = '2.45rem';
+toggleButton.textContent = '▶';
+
+toggleButton.addEventListener('click', toggleSidePanel);
