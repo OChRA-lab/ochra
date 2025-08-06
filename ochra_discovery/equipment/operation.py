4dabec4cc11b12ad7acf5b21b784993c26a87f01
acedwards7 <adamcedwards1@gmail.com>
2025-08-06 13:26:49 +0100

    added helper methods to get data and get operation_result object

--- ochra_discovery/equipment/operation.py
+++ ochra_discovery/equipment/operation.py
@@ -1,4 +1,5 @@
 from ochra_common.equipment.operation import Operation
+from ochra_common.equipment.operation_result import OperationResult
 from ochra_common.utils.mixins import RestProxyMixinReadOnly
 from uuid import UUID
 
@@ -12,3 +13,23 @@ class Operation(Operation, RestProxyMixinReadOnly):
         """
         super().__init__()
         self._mixin_hook(self._endpoint, object_id)
+
+    def get_result_object(self) -> OperationResult:
+        """Get the result of the operation
+
+        Returns:
+            OperationResult: The result of the operation
+        """
+        return OperationResult(id=self.result)
+
+    def get_result_data(self, path: str = None) -> bytes:
+        """Get the data of the operation result, optionally saving it to a file.
+
+        Returns:
+            bytes: The data of the operation result
+        Args:
+            path (str): The path to save the data to. If None, does not save the data.
+        """
+        if path is not None:
+            self.get_result().save_data(path)
+        return self._lab_conn.get_data("operation_results", self.result)
